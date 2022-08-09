c99da23934e41d3a29f4b40dab72ba8ab36bbc89
Koushik16pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 16:42:22 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+class A
+{
+
+static int C = 40;
+int a = 15 ;
+public static void main(String ...args)
+{
+A obj = new A();
+
+int b=18 ;
+
+ System.out.println(A.C);
+ System.out.println(b);
+ System.out.println(obj.a);
+}
+
+}
