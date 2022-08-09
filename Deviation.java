0263a85cd4aafb0e2e3819e0fb99f1acfbafef75
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 19:17:39 +0530

    Add files via upload

---
+++ Deviation.java
@@ -0,0 +1,9 @@
+class Deviation{
+public static void main(String ...args){
+System.out.println("The two number is :");
+int a = 14; // initialize 1st local variable
+int b= 2; // initialize 2nd local variable
+System.out.println(a + " " + b);
+int deviation = a/b; // logic
+System.out.println("The deviation is :" + deviation);
+}}
