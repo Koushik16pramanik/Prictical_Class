e67d042807330b35a161190f0e72a3eda7888409
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 19:04:26 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+class Subtraction{
+public static void main(String ...args){
+System.out.println("The two number is :");
+int a = 15; // initialize 1st local variable
+int b= 6; // initialize 2nd local variable
+System.out.println(a + " " + b);
+int subtraction = a-b; // logic
+System.out.println("The subtraction is :" + subtraction);
+}}
