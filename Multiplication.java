181cf1e381f35ec548532f5bea5dbeb023e1513a
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 19:11:19 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,9 @@
+class Multiplication{
+public static void main(String ...args){
+System.out.println("The two number is :");
+int a = 15; // initialize 1st local variable
+int b= 12; // initialize 2nd local variable
+System.out.println(a + " " + b);
+int multiplication = a*b; // logic
+System.out.println("The multiplication is :" + multiplication);
+}}
