790e567ab941ea152dd1bdbabd32a0ebc4bae97b
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 18:48:34 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+class Addition{
+public static void main(String ...args){
+System.out.println("The two number is :");
+int a = 8; // initialize 1st local variable
+int b= 5; // initialize 2nd local variable
+System.out.println(a + " " + b);
+int sum = a+b; // logic
+System.out.println("The sum is :" + sum);
+}}
