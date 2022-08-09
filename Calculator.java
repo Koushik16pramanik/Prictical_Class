2385d4a0bf597bce421c392ce59c19a37789e66e
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-09 19:30:26 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+class Calculator{
+public static void main(String ...args){
+System.out.println("The two number is :");
+int a = 30; // initialize 1st local variable
+int b= 10; // initialize 2nd local variable
+System.out.println(a + " " + b);
+int sum = a+b; // logic
+int subtraction = a-b; // logic
+int multiplication = a*b; // logic
+int deviation = a/b; // logic
+System.out.println("The sum is :" + sum);
+System.out.println("The subtraction is :" + subtraction);
+System.out.println("The multiplication is :" + multiplication);
+System.out.println("The deviation is :" + deviation);
+
+}}
