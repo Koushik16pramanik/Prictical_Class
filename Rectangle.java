2e6652f34ae10f25b222c6a0757e4659cf71b957
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-10 16:06:30 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle{
+public static void main(String ...args){
+System.out.println("The area and perimeter :");
+int a = 7; // initialize a is length
+int b= 5; // initialize b is breadth
+System.out.println(a + " " + b);
+int area = a*b; // logic
+int perimeter = 2*(a+b); // logic
+System.out.println("The area is :" + area);
+System.out.println("The perimeter is :" + perimeter);
+}}
---
+++ Square.java
@@ -0,0 +1,10 @@
+ class Square{
+public static void main(String ...args){
+System.out.println("The area and a perimeter :");
+int a = 8; // initialize a is side length
+System.out.println(a + " " + a);
+int area = a*a; // logic
+int perimeter = 4*a; // logic
+System.out.println("The area is :" + area);
+System.out.println("The perimeter is :" + perimeter);
+}}
