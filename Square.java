44e733687d878bf2bf080ac1aa69f397d0d54d7d
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-10 18:35:29 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -3,8 +3,8 @@ public static void main(String ...args){
 System.out.println("The area and a perimeter :");
 int a = 8; // initialize a is side length
 System.out.println(a + " " + a);
-int area = a*a; // logic
-int perimeter = 4*a; // logic
+int area = a*a;
+int perimeter = 4*a;
 System.out.println("The area is :" + area);
 System.out.println("The perimeter is :" + perimeter);
 }}
