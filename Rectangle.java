354bf768ae949fd8dc153443f1c3250106c06756
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-10 18:34:31 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -4,8 +4,8 @@ System.out.println("The area and perimeter :");
 int a = 7; // initialize a is length
 int b= 5; // initialize b is breadth
 System.out.println(a + " " + b);
-int area = a*b; // logic
-int perimeter = 2*(a+b); // logic
+int area = a*b;
+int perimeter = 2*(a+b);
 System.out.println("The area is :" + area);
 System.out.println("The perimeter is :" + perimeter);
 }}
