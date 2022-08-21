b999110eb622619c1f829a68f2a922105d061d92
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-21 12:28:59 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,18 @@
+package Oerateor;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		int a = 7;
+		int b= 4;
+		if(a+b < 10) {
+		System.out.println("a + b is less than 10");
+		}
+		else
+		{
+			System.out.println("a + b is greater than 10");
+		}
+
+	}
+
+}
---
+++ Ifelseladder.java
@@ -0,0 +1,19 @@
+package Oerateor;
+
+public class Ifelseladder {
+
+	public static void main(String[] args) {
+		String name = "Koushik";
+		if(name == "Tanmay") {
+		System.out.println("name is Tanmay");
+		}else if (name == "Akash") {
+		System.out.println("name is Akash");
+		}else if (name == "Repan") {
+		System.out.println("name is Repan");
+		}else {
+		System.out.println(name);
+		}
+
+	}
+
+}
