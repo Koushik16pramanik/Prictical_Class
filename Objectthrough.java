e9d6456cfcb8e9b4e2a85f9469ea4baa690a4131
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-30 16:45:52 +0530

    Add files via upload

---
+++ Objectthrough.java
@@ -0,0 +1,25 @@
+package Oerateor;
+
+public class Objectthrough {
+// field
+	int age;
+	String name;
+		}
+//create another class
+		class objectthrough1{
+			//main method
+			
+	
+	public static void main(String[] args) {
+		//creating object
+		Objectthrough obr=new Objectthrough();
+		obr.age=20;
+		obr.name="Koushik";
+		//print it
+		System.out.println(obr.age+" "+obr.name);
+	
+			
+
+	}
+
+}
