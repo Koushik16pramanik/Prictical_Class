7ff368aa96fcd6df49046a110a034875566df609
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-30 17:35:48 +0530

    Add files via upload

---
+++ Defaultcon1.java
@@ -0,0 +1,14 @@
+package Oerateor;
+
+public class Defaultcon1 {
+			
+		Defaultcon1() {
+			System.out.println("constructor is created");
+		}
+		
+	public static void main(String[] args) {
+		Defaultcon1 d=new Defaultcon1();
+
+	}
+
+}
