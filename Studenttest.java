7b0db71c9d3bceadf27cd03d0364ce4e3186b3ab
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-08-30 16:46:37 +0530

    Add files via upload

---
+++ Studenttest.java
@@ -0,0 +1,22 @@
+package Oerateor;
+
+public class Studenttest {
+
+	//field
+		int age;
+		String name;
+}
+		// create another class
+		class Studenttest1{
+		//main method
+		public static void main(String[] args) {
+			//creating object
+			Studenttest st=new Studenttest();
+			// print it
+			System.out.println(st.age);
+			System.out.println(st.name);
+			
+
+	}
+
+}
