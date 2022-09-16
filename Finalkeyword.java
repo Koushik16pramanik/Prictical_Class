0a5bf3c9e86fc23575b97a5eae3db8e57fe4b810
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-16 18:20:30 +0530

    Add files via upload

---
+++ Finalkeyword.java
@@ -0,0 +1,14 @@
+package Oerateor;
+
+public class Finalkeyword {
+final int a=50;
+void method() {
+	int a=70;
+	System.out.println(a);
+}
+	public static void main(String[] args) {
+	Finalkeyword fk=new Finalkeyword();
+	fk.method();
+	}
+
+}
