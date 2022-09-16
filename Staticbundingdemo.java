3fd52fdffbfa893203bb3aa1a6e8d088c36f466c
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-16 17:10:20 +0530

    Add files via upload

---
+++ Staticbundingdemo.java
@@ -0,0 +1,16 @@
+package Oerateor;
+/*variable have a type
+public class Staticbundingdemo {
+int a=30; //ais a integer type of variable
+//Reference have a type
+ * Staticbundingdemo obj=new Staticbundingdemo();
+ */
+	public class Staticbundingdemo{
+private void show() {
+	System.out.println("statiic bunding");
+}
+public static void main(String[] args) {
+	Staticbundingdemo obj1=new Staticbundingdemo();
+obj1.show();
+}
+}
