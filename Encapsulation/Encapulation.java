7e7e446fb8dfe83c1716ab5407adac00579b5c93
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-20 17:30:54 +0530

    Update Encapulation.java

--- Encapsulation/Encapulation.java
+++ Encapsulation/Encapulation.java
@@ -1,13 +1,13 @@
 package Activity;
 //part- 1 of Encapsulation.
-// fully encapsulated class
+// fully encapsulated class.
 public class Encapulation {
 private String name;
-// for get the name
+// for get the name.
 public String getName() {
 	return name;
 	}
-// for set the name
+// for set the name.
 public void setName(String name) {
 	this.name=name;
 	System.out.println("name sucessfully updated");
