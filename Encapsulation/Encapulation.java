9f9181a7975436617ee997aa707a88f45133cc83
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-20 17:29:45 +0530

    Add files via upload

---
+++ Encapsulation/Encapulation.java
@@ -0,0 +1,14 @@
+package Activity;
+//part- 1 of Encapsulation.
+// fully encapsulated class
+public class Encapulation {
+private String name;
+// for get the name
+public String getName() {
+	return name;
+	}
+// for set the name
+public void setName(String name) {
+	this.name=name;
+	System.out.println("name sucessfully updated");
+}}
---
+++ Encapsulation/Encapulationmainmethod.java
@@ -0,0 +1,10 @@
+package Activity;
+//part- 2 Encapsulation.
+public class Encapulationmainmethod {
+	public static void main(String[] args) {
+		Encapulation encapulation=new Encapulation();
+		//set the value with the help of name variable.
+		encapulation.setName("Koushik");
+		//get the value with the help of name variable
+		System.out.println(encapulation.getName());
+	}}
