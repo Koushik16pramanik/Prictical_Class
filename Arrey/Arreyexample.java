ac7816688bd13464e86a6c4e2b40ffe77fd2fff0
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-09-29 13:59:36 +0530

    Add files via upload

---
+++ Arrey/Arreyexample.java
@@ -0,0 +1,11 @@
+package Activity;
+
+public class Arreyexample {
+	public static void main(String[] args) {
+		display(new int[]{ 10,20,30,40,50});//passing anonymous array
+		}
+	//no need to declare the array while passing an array to the method
+			 static void display(int arr[]) {
+			 for(int i=0;i<=arr.length-1;i++)
+					System.out.println(arr[i]);
+			}}
