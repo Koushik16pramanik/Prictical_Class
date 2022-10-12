88795529e576935eb29e89990faf0917487d7017
KOUSHIK PRAMANIK <110465853+Koushik16pramanik@users.noreply.github.com>
2022-10-12 10:48:28 +0530

    Add files via upload

---
+++ String/StudentTest.java
@@ -0,0 +1,13 @@
+package String;
+
+ class StudentTest {
+	 
+	 static // field
+	 String name= "Hello";
+	 // main method
+	public static void main(String[] args) {  //  creating object
+		System.out.println("substring is "+ name.substring(0,2)); // start index end index
+		// he //h
+		System.out.println("substring is "+ name.substring(2));
+		// llo  // lo
+		}}
