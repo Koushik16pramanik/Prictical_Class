06c0fb90c7dbacf784039c39d9ba38264183ef8c
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-11-02 17:32:10 +0530

    Add files via upload

---
+++ Multithreaing/DemonThread.java
@@ -0,0 +1,20 @@
+package Multithreaing;
+
+public class DemonThread extends Thread {
+	public void run() {
+		if (Thread.currentThread().isDaemon()) {
+		System.out.println(Thread.currentThread().getName());
+		System.out.println("I am Demon");
+	}	else {
+		System.out.println("I am user");
+	}}
+	public static void main(String[] args) {
+		DemonThread t = new DemonThread();
+		DemonThread t1= new DemonThread();
+		DemonThread t2 = new DemonThread();
+		
+		t1.setDaemon(true);
+		t.start();
+		t1.start();
+		 t2.start();
+	}}
