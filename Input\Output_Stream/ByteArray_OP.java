cfcd7e02ea5254d6337c4ec5d9727dbcf6b1a52e
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-11-09 17:24:01 +0530

    Add files via upload

---
+++ Input\Output_Stream/ByteArray_OP.java
@@ -0,0 +1,18 @@
+package IO;
+import java.io.*;
+
+public class ByteArray_OP {
+	public static void main(String[] args) {
+		try {
+			FileOutputStream f= new FileOutputStream("D:\\test.txt");
+			FileOutputStream f1= new FileOutputStream("G:\\test.txt");
+			ByteArrayOutputStream bos= new ByteArrayOutputStream();
+			bos.write(65);
+			bos.writeTo(f);
+			bos.writeTo(f1);
+			bos.close();
+			System.out.println("done");
+	}
+		catch(Exception e) {
+			System.out.println(e);
+		}}}
