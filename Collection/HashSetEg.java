c02e9f02483662de068ac865fea26e924bc764f2
Koushik Pramanik <110465853+Koushik16pramanik@users.noreply.github.com>
2022-11-18 18:11:37 +0530

    Add files via upload

---
+++ Collection/HashSetEg.java
@@ -0,0 +1,20 @@
+package Collection;
+import java.util.*;
+// 1hs inherits the hasset class & implements the set interface
+class HashSetEg {
+	 // allowing null value
+	public static void main(String[] args) {
+		Set<String> s4= new HashSet<String>();
+		s4.add("Pen");
+		s4.add("Pencil");
+		s4.add("Mouse");
+		s4.add("Phone");
+		s4.add(null);
+		s4.add("AC");
+		s4.add("Bottle");
+		s4.add("Earphone");
+		s4.add("Pen");
+		Iterator<String> itr= s4.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}}}
